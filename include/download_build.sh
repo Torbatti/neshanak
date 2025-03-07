b2e18d1723e6b8bda64d6544a9c9422db8715759
torbatti <torbattii@gmail.com>
2025-03-07 22:12:53 +0330

    download bins scripts

---
+++ include/download_build.sh
@@ -0,0 +1,2 @@
+whet https://github.com/a-h/templ/releases/download/v0.3.833/templ_Linux_x86_64.tar.gz
+wget https://downloads.sqlc.dev/sqlc_1.28.0_linux_amd64.tar.gz
---
+++ include/download_deploy.sh
@@ -0,0 +1,1 @@
+wget https://github.com/pressly/goose/releases/download/v3.24.1/goose_linux_x86_64
