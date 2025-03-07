315217dd5f1df4c5f134c6ff401c6e241d426a1a
torbatti <torbattii@gmail.com>
2025-03-07 20:44:57 +0330

    add: basic commands

--- cmd/superuser.go
+++ cmd/superuser.go
@@ -1,1 +1,17 @@
 package cmd
+
+import (
+	"github.com/Torbatti/neshanak/core"
+	"github.com/spf13/cobra"
+)
+
+// NewSuperuserCommand creates and returns new command for managing
+// superuser accounts (create, update, upsert, delete).
+func NewSuperuserCommand(app core.App) *cobra.Command {
+	command := &cobra.Command{
+		Use:   "superuser",
+		Short: "Manage superusers",
+	}
+
+	return command
+}
